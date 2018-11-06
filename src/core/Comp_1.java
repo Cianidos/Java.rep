1e7bff40b1059ae05a897eeaab5f2a446a0b81c8
Cianidos <nikitosiks338@gmail.com>
2018-11-06 16:19:18 +0300

    Создано ядро и два компа.

---
+++ src/core/Comp_1.java
@@ -0,0 +1,7 @@
+package core;
+
+public class Comp_1 {
+    public static void main(String[] args) {
+        System.out.print("Hello World!!");
+    }
+}
---
+++ src/core/Comp_2.java
@@ -0,0 +1,7 @@
+package core;
+
+public class Comp_2 {
+    public static void main(String[] args) {
+        System.out.print("Hello World2");
+    }
+}
